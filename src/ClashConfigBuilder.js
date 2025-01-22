daea6a980224280a65058adbff5cf5387887115f
fly2andromeda <dova99k@gmail.com>
2025-01-22 23:49:39 +0800

    修改名称

--- src/ClashConfigBuilder.js
+++ src/ClashConfigBuilder.js
@@ -35,7 +35,7 @@ export class ClashConfigBuilder extends BaseConfigBuilder {
         const proxyList = this.config.proxies.map(proxy => proxy.name);
         
         this.config['proxy-groups'].push({
-            name: '⚡ 自动选择',
+            name: '⚡ 延迟优先',
             type: 'url-test',
             proxies: DeepCopy(proxyList),
             url: 'https://www.gstatic.com/generate_204',
@@ -43,15 +43,15 @@ export class ClashConfigBuilder extends BaseConfigBuilder {
             lazy: false
         });
 
-        proxyList.unshift('DIRECT', 'REJECT', '⚡ 自动选择');
-        outbounds.unshift('🚀 节点选择');
+        proxyList.unshift('DIRECT', 'REJECT', '⚡ 延迟优先');
+        outbounds.unshift('🚀 手动选择');
         
         outbounds.forEach(outbound => {
-            if (outbound !== '🚀 节点选择') {
+            if (outbound !== '🚀 手动选择') {
                 this.config['proxy-groups'].push({
                     type: "select",
                     name: outbound,
-                    proxies: ['🚀 节点选择', ...proxyList]
+                    proxies: ['🚀 手动选择', ...proxyList]
                 });
             } else {
                 this.config['proxy-groups'].unshift({
@@ -67,15 +67,15 @@ export class ClashConfigBuilder extends BaseConfigBuilder {
                 this.config['proxy-groups'].push({
                     type: "select",
                     name: rule.name,
-                    proxies: ['🚀 节点选择', ...proxyList]
+                    proxies: ['🚀 手动选择', ...proxyList]
                 });
             });
         }
 
         this.config['proxy-groups'].push({
             type: "select",
-            name: "🐟 漏网之鱼",
-            proxies: ['🚀 节点选择', ...proxyList]
+            name: "主要流量",
+            proxies: ['🚀 手动选择', ...proxyList]
         });
     }
     formatConfig() {
@@ -91,7 +91,7 @@ export class ClashConfigBuilder extends BaseConfigBuilder {
         });
 
         // Add the final catch-all rule
-        this.config.rules.push('MATCH,🐟 漏网之鱼');
+        this.config.rules.push('MATCH,主要流量');
 
         return yaml.dump(this.config);
     }
